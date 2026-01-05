a3a61b897e00e8d5996a9b43ae27bcdd03750752
Peter Muraya <sammypeter1944@gmail.com>
2026-01-05 15:07:38 +0300

    feat: add routing and routes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,34 @@
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Instructor from './components/Instructor';
-import About from './components/About';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import AboutPage from './pages/AboutPage';
+import GalleryPage from './pages/GalleryPage';
+import InstructorPage from './pages/InstructorPage';
+import ContactPage from './pages/ContactPage';
 import Ranks from './components/Ranks';
 import Programs from './components/Programs';
-import Gallery from './components/Gallery';
 import Locations from './components/Locations';
 import Testimonials from './components/Testimonials';
-import CTA from './components/CTA';
-import Footer from './components/Footer';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="w-full bg-black">
+    <div className="w-full bg-black pt-16 min-h-screen">
       <Navbar />
-      <Hero />
-      <Instructor />
-      <About />
-      <Ranks />
-      <Programs />
-      <Gallery />
-      <Locations />
-      <Testimonials />
-      <CTA />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/instructor" element={<InstructorPage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        {/* Additional standalone pages that reuse components */}
+        <Route path="/programs" element={<Programs />} />
+        <Route path="/locations" element={<Locations />} />
+        <Route path="/ranks" element={<Ranks />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+      </Routes>
+
       <Footer />
     </div>
   );
