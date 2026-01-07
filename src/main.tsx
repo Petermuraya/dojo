2c9888749455c65db47a2b57ac0932ef40e5399e
Peter Muraya <sammypeter1944@gmail.com>
2026-01-07 06:13:35 +0300

    chore(main): wrap app with SupabaseProvider and HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+import { SupabaseProvider } from './contexts/SupabaseProvider';
 
 // Use HashRouter for GitHub Pages so refreshes do not 404
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <SupabaseProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </SupabaseProvider>
   </StrictMode>
 );
